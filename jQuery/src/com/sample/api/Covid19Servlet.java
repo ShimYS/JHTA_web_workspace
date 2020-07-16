4567e2dcb9a3c8c3418a034d5d36e23bbe78ac42
ShimYS <sim5522@naver.com>
2020-07-16 17:51:45 +0900

    2020-07-16 jQuery-ajax와 covid 공공데이터 활용하여 coivd 현황 페이지 구현 연습

---
+++ jQuery/src/com/sample/api/Covid19Servlet.java
@@ -0,0 +1,70 @@
+package com.sample.api;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+
+@WebServlet(urlPatterns = "/covid19sido")
+public class Covid19Servlet extends HttpServlet {
+	
+	private String serviceURL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+	private String serviceKey = "cMSjn%2BFeFTqFgPDX4RtfrXOs8vTKizs%2BK5Dtp0rOE8GJpaNMkVS6azOw4PX6nshqnm%2FL%2FyZlUhp7mKQ2JSKQbw%3D%3D";
+	private int numOfRows = 10;
+	
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		int pageNo = Integer.parseInt(req.getParameter("pageNo"));
+		String startCreateDt = req.getParameter("startCreateDt");
+		String endCreateDt = req.getParameter("endCreateDt");
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append(serviceURL);
+		builder.append("?serviceKey="+serviceKey);
+		builder.append("&pageNo="+pageNo);
+		builder.append("&numOfRows="+numOfRows);
+		builder.append("&startCreateDt="+startCreateDt);
+		builder.append("&endCreateDt="+endCreateDt);
+		
+		URL url = new URL(builder.toString());
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Content-type", "text/xml");
+		
+		System.out.println("응답코드: "+connection.getResponseCode());
+		
+		IOUtils.copy(connection.getInputStream(), resp.getOutputStream());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
